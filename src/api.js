33077f56b4adb48dcdcc24a340400d6b3c08512f
Abdullah Gira <abdullaho.gira@gmail.com>
2022-06-20 15:58:40 +0200

    Starter template ready to use with auth

---
+++ src/api.js
@@ -0,0 +1,7 @@
+const api = {
+  auth: {
+    refreshToken: "/PATH_TO_REFRESH_TOKEN",
+  },
+};
+
+export default api;
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import { Button } from "semantic-ui-react";
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world! <Button basic>👋</Button>
+    </h1>
+  );
+}
+
+export default App;
