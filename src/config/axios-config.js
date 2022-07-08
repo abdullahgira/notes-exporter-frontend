0558cc193ab01c64b0db0be1c691587d4481ba33
Abdullah Gira <abdullaho.gira@gmail.com>
2022-07-08 13:58:12 +0200

    Startup cleanup

--- src/config/axios-config.js
+++ src/config/axios-config.js
@@ -1,21 +1,8 @@
 import Axios from "axios";
-import auth from "../utils/auth";
 
 Axios.defaults.baseURL =
   process.env.NODE_ENV !== "production"
     ? process.env.REACT_APP_DEV_URL
-    : process.env.REACT_APP_SERVER_URL;
-
-const AuthAxios = Axios.create();
-
-AuthAxios.interceptors.request.use(async (config) => {
-  let accessToken = await auth.getToken();
-  config.headers = {
-    Authorization: `Bearer ${accessToken}`,
-    "Accept-Language": window.localStorage.getItem("language"),
-  };
-  return config;
-});
+    : process.env.REACT_APP_PROD_URL;
 
 export const axios = Axios;
-export const authAxios = AuthAxios;
